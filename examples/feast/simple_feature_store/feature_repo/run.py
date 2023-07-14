cc4b84aa5c8cc218d78737cea64398195f3a0477
zilto <tjean@DESKTOP-V6JDCS2.localdomain>
2023-07-14 16:15:28 -0400

    first commit Feast

---
+++ examples/feast/simple_feature_store/feature_repo/run.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+
+from hamilton import driver, base
+
+import store_definitions
+import store_operations
+
+from demo_inputs import (
+    HISTORICAL_ENTITY_DF,
+    HISTORICAL_FEATURES,
+    ONLINE_ENTITY_ROWS,
+    ONLINE_FEATURES,
+    STREAM_EVENT_DF,
+)
+
+
+def main():
+    dr = driver.Driver(
+        dict(
+            feast_repository_path=".",
+            feast_config={}
+        ),
+        store_operations,
+        store_definitions,
+        adapter=base.SimplePythonGraphAdapter(base.DictResult())
+    )
+
+    final_vars = [
+        "apply"
+        # "driver_activity_v1_fs",
+    ]
+
+    inputs = dict(
+        driver_source_path="./data/driver_stats.parquet",
+        end_date=datetime.now(),
+        entity_df=HISTORICAL_ENTITY_DF,
+        historical_features_=HISTORICAL_FEATURES,
+        batch=True
+    )
+
+    out = dr.execute(final_vars=final_vars, inputs=inputs)
+
+    # dr.display_all_functions("definitions", {"format": "png"})
+    # dr.visualize_execution(final_vars, "exec", {"format": "png"}, inputs=inputs)
+
+    print(out.keys())
+
+
+if __name__ == "__main__":
+    main()
+    
